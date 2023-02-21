ef93a48e233f4e6a8d6e48167ee573c3dcb2bc95
Taowuu <taowuu@foxmail.com>
2023-02-21 09:08:06 +0800

    抽离 get set

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,46 +1,61 @@
-import { track } from './effect'
-import { trigger } from './effect'
+import { mutableHandlers, readonlyHandlers } from './baseHandlers'
+// import { track } from './effect'
+// import { trigger } from './effect'
 
-function createGetter(isReadonly = false) {
-  return function get(target, key) {
-    const res = Reflect.get(target, key)
-    if(!isReadonly) {
-      // 依赖收集
-      track(target, key)
-    }
-    return res
-  }
-}
+// function createGetter(isReadonly = false) {
+//   return function get(target, key) {
+//     const res = Reflect.get(target, key)
+//     if(!isReadonly) {
+//       // 依赖收集
+//       track(target, key)
+//     }
+//     return res
+//   }
+// }
+
+// function createSetter(isReadonly = false) {
+//   return function set(target, key, value) {
+//     const res = Reflect.set(target, key, value)
+//     // 触发依赖
+//     trigger(target, key)
+//     return res
+//   }
+// }
 
 export function reactive(raw) {
-  return new Proxy(raw, {
+  return new Proxy(raw, mutableHandlers)
+  
+  // return new Proxy(raw, {
     // get(target, key) {
     //   const res = Reflect.get(target, key)
     //   // 依赖收集
     //   track(target, key)
     //   return res
     // },
-    get: createGetter(),
+    // get: createGetter(),
 
-    set(target, key, value) {
-      const res = Reflect.set(target, key, value)
-      // 触发依赖
-      trigger(target, key)
-      return res
-    }
-  })
+    // set(target, key, value) {
+    //   const res = Reflect.set(target, key, value)
+    //   // 触发依赖
+    //   trigger(target, key)
+    //   return res
+    // }
+    // set: createSetter(),
+  // })
 }
 
 export function readonly(raw) {
-  return new Proxy(raw, {
-    // get(target, key) {
-    //   const res = Reflect.get(target, key)
-    //   return res
-    // },
-    get: createGetter(true),
+  return new Proxy(raw, readonlyHandlers)
 
-    set(target, key, value) {
-      return true
-    }
-  })
+  // return new Proxy(raw, {
+  //   // get(target, key) {
+  //   //   const res = Reflect.get(target, key)
+  //   //   return res
+  //   // },
+  //   get: createGetter(true),
+
+  //   set(target, key, value) {
+  //     return true
+  //   }
+  // })
 }
